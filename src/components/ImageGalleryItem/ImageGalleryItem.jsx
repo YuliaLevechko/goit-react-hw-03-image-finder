e6d60e72895e0f78f36bc8362a332f505399d68d
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2024-01-17 17:13:37 +0200

    comit

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import css from  './ImageGalleryItem.module.css';
+import Modal from '../Modal';
+
+class ImageGalleryItem extends Component {
+  state = {
+    shownModal: false,
+  };
+  onModal = () => {
+    this.setState(({ shownModal }) => ({ shownModal: !shownModal }));
+  };
+  render() {
+    const { item } = this.props;
+    const { webformatURL } = item;
+    return (
+      <li className={css.gallery_item}>
+        <img
+          onClick={this.onModal}
+          className={css.gallery_img}
+          src={webformatURL}
+          alt="img"
+        />
+        {this.state.shownModal && <Modal onClose={this.onModal} image={item} />}
+      </li>
+    );
+  }
+}
+
+ImageGalleryItem.propTypes = {
+  item: PropTypes.object,
+};
+
+export default ImageGalleryItem;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Notiflix from 'notiflix';
+
+import css from  './Searchbar.module.css'
+
+ class Searchbar extends Component {
+  state = {
+    inputvalue: ''
+  }
+  hendleSubmit = (e)=>{
+    e.preventDefault () 
+    if( this.state.inputvalue.trim()=== ''){
+       Notiflix.Notify.failure('please writing value ');
+    }
+  
+    this.props.onSubmit(this.state.inputvalue)
+    this.setState({
+      inputvalue: ''
+    })
+  }
+
+  hendleChange = (e) =>{
+    this.setState({
+      inputvalue: e.currentTarget.value.toLowerCase()
+    })
+  }
+  render() {
+  
+    return (
+        <header className={css.searchbar}>
+        <form  onSubmit={ this.hendleSubmit}   className={css.form}>
+          <input  
+          onChange={this.hendleChange}
+            className={css.input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+            <button type="submit" className={css.button}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1200px-Search_Icon.svg.png" width="30px" height="30px" alt=""/>
+          </button>
+        </form>
+      </header>
+    )
+  }
+}
+
+export default Searchbar
